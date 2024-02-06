561e6d8f2300bb42a8cccb0de5d41482d06b9f7a
Victor Mello <victorsmello93@gmail.com>
2024-02-05 23:02:03 -0300

    First commit

---
+++ AuctionApp/src/AuctionApp.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -0,0 +1,17 @@
+﻿using AuctionApp.API.Entities;
+
+namespace AuctionApp.API.UseCases.Auctions.GetCurrent
+{
+    public class GetCurrentAuctionUseCase
+    {
+        public Auction Execute()
+        {
+            return new Auction
+            {
+                Id = 1,
+                Ends = DateTime.Now,
+                Starts = DateTime.Now,
+            };
+        }
+    }
+}
